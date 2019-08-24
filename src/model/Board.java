7510b28af48295e35d7a5a0fe63458d02174fc07
Oss21 <osvan202@gmail.com>
2019-08-24 10:11:05 -0500

    first

---
+++ src/model/Board.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Board {
+	/**
+	 * Número primo.
+	 */
+	private boolean primeNumber;
+	/**
+	 * Número de filas de la matriz
+	 */
+	private int row;
+	/**
+	 * Número de columnas de la matriz.
+	 */
+	private int column;
+	
+	/**
+	 * Constructor de la clase tablero
+	 * @param row Número de filas.
+	 * @param column Número de columnas.
+	 */
+	public Board(int row, int column) {
+		this.row = row;
+		this.column = column;
+		this.primeNumber = false;
+	}
+	
+	public void createCoefficienteMatrix(boolean repeatedNumbers, int row , int column ) {
+		
+	}
+	
+
+	
+
+}

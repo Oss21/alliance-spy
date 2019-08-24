b05657a4b2ea5896041bb214aea9ef97f33a4f29
Oss21 <osvan202@gmail.com>
2019-08-24 14:43:14 -0500

    Second

--- src/model/Board.java
+++ src/model/Board.java
@@ -24,10 +24,62 @@ public class Board {
 		this.column = column;
 		this.primeNumber = false;
 	}
-	
-	public void createCoefficienteMatrix(boolean repeatedNumbers, int row , int column ) {
+
+	public void createRandomMatrix(boolean repeatedNumbers, int row , int column,int [][] matrix ) {
+		
+		//Initialize the Matrix with its rows and columns.
+		matrix = new int [row][column] ;
+		
+			if(repeatedNumbers) {
+				int number = 0;	
+					for (int i = 0; i < matrix.length; i++) {
+						for (int j = 0; j < matrix[i].length; j++) {
+							//Random numbers from 1 to 20.
+							number = (int) (Math.random()*20)+1;	
+							matrix[i][j] = number;
+						}
+					}
+			}else {
+				int size = row*column;
+				int [] matrixTemp = randomArray(size);
+				int count = 0;
+				for (int i = 0; i < matrix.length; i++) {
+					for (int j = 0; j < matrix[i].length; j++) {
+						//Random numbers from 1 to 20.
+						matrix[i][j] =matrixTemp[count++];
+					}
+				}
+			}
+			
 		
 	}
+	
+	/**
+	 * Permite crear un arreglo de numeros aleatorios sin repetir
+	 * @param size tamaño del arreglo.
+	 */
+	private int[] randomArray(int size) {
+			int[] randomNumbers = new int[size];
+				for (int i = 0; i < randomNumbers.length; i++) {
+					int number = (int) (Math.random()*size+1);
+					randomNumbers[i] = number;
+				}
+				for (int i = 0; i < randomNumbers.length; i++) {
+					for (int j = 0; j < randomNumbers.length; j++) {
+						if(randomNumbers[i] == randomNumbers[j] && i!=j) {
+							int number = (int) (Math.random()*size+1);
+							randomNumbers[i] = number;
+							 i = 0;
+						}
+					}
+				}
+				for (int i = 0; i < randomNumbers.length; i++) {
+					System.out.print(randomNumbers[i]+ " ");
+				} 
+			
+		return randomNumbers;
+	}
+
 
 
 

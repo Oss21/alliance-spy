620a1a4215440ff4bb522c22a4cca6a22c4bbb38
EliasE12 <simply753@gmail.com>
2019-08-24 11:34:02 -0500

    Reparado el problema de la librerpia jfoenix-8.0.8.jar

--- src/controlView/AllianceSpyController.java
+++ src/controlView/AllianceSpyController.java
@@ -4,14 +4,32 @@ import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class AllianceSpyController implements Initializable {
 
+    @FXML private Pane pBanner;
+    @FXML private JFXButton btClassicalMultiplication;
+    @FXML private JFXButton btMultiplicationByBlocks;
+    @FXML private JFXButton btMultiplicationLinearCombination;
+    @FXML private Label lbNoti;
+    @FXML private JFXTextField tfRows;
+    @FXML private JFXTextField tfColum;
+    @FXML private JFXButton btDifferent;
+    @FXML private JFXButton btRepeated;
+    @FXML private JFXButton btGenerate;
+    @FXML private Pane pPlaceMatrix;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
 
 
 }
